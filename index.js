ceba1109bbf51bfde82d592fcdc483eb40dd8e06
Lawtrel <kazemarus@outlook.com>
2025-01-18 11:51:07 -0300

    listar usuarios de um json

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 let express = require("express");
 let app = express();
 
+app.use(express.json());
+
+let users = [
+    {id: 1, name: "Lawtrel" , email : "lawtrel@lawtrel.com"},
+    {id: 2, name: "Leo" , email: "leo@leo.com"},
+    {id: 3, name: "Law", email: "law@law.com"}
+];
+
+app.get('/users', (req, res) => {
+    return res.json(users);
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(users => users.id === id);
+    const status = user ? 200 : 404;
+    return res.status(status).json(user);
+})
+
 app.get('/', (req, res) => {
     return res.send('Servidor Iniciado!');
     res.json({
